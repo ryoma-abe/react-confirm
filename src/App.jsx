81189f2381f4e031355688e02d64f655dbd1101e
ryoma-abe <liaozhenanbu387@gmail.com>
2025-06-01 12:00:22 +0900

    不要なCSSファイルを削除し、App.jsxにデータ取得機能を追加しました。テキスト入力とボタンを実装しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [flg, setFlg] = useState(false);
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Fetchエラー:", err));
+  };
+  useEffect(() => {
+    fetchData();
+  }, [flg]);
+
   return (
     <>
       <p>テスト</p>
+
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={() => setFlg(!flg)}>取得</button>
     </>
   );
 }
