83e4fc98dab5443562525e928b9bed7906009df8
ryoma-abe <liaozhenanbu387@gmail.com>
2025-06-02 19:51:27 +0900

    App.jsxを更新し、カウント機能を追加しました。データ取得機能とテキスト入力を削除し、ボタンでカウントを増減できるようにしました。

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,22 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
-  const [flg, setFlg] = useState(false);
-  const fetchData = () => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Fetchエラー:", err));
-  };
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    fetchData();
-  }, [flg]);
-
+    document.title = "テスト";
+  });
+  const onClickUp = () => {
+    setCount(count + 1);
+  };
+  const onClickDown = () => {
+    setCount(count - 1);
+  };
   return (
-    <>
-      <p>テスト</p>
-
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={() => setFlg(!flg)}>取得</button>
-    </>
+    <div>
+      <button onClick={onClickUp}>+</button>
+      {count}
+      <button onClick={onClickDown}>-</button>
+    </div>
   );
 }
 
