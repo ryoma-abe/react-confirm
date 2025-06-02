eacb8542cbc9ffbed60f596f6c78c5251bb3af9b
ryoma-abe <liaozhenanbu387@gmail.com>
2025-06-02 20:34:38 +0900

    App.jsxに名前入力フォームを追加し、送信時にアラートでフルネームを表示する機能を実装しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [fName, setFname] = useState("");
+  const [lName, setLname] = useState("");
+
+  const onSubmit = () => {
+    alert(`${fName} ${lName}`);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>
+          FirstName
+          <input
+            type="text"
+            value={fName}
+            onChange={(e) => setFname(e.target.value)}
+          />
+        </label>
+        <label>
+          LastName
+          <input
+            type="text"
+            value={lName}
+            onChange={(e) => setLname(e.target.value)}
+          />
+        </label>
+        <button type="submit">送信</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
