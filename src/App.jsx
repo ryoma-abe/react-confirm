ad19d19b378a6c666df43af48424cd2bd5cc9330
ryoma-abe <liaozhenanbu387@gmail.com>
2025-06-03 20:30:51 +0900

    package.jsonとpackage-lock.jsonにreact-router-domを追加し、App.jsxを空のコンポーネントに変更しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
 import "./App.css";
-import useHook from "./useHook";
 function App() {
-  const [lName, setLname] = useHook("");
-  const [fName, setFname] = useHook("");
-
-  const onSubmit = () => {
-    alert(`${fName} ${lName}`);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <label>
-          FirstName
-          <input type="text" {...setLname} />
-        </label>
-        <label>
-          LastName
-          <input type="text" {...setFname} />
-        </label>
-        <button type="submit">送信</button>
-      </form>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
